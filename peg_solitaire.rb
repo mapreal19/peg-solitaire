f5ab627d9bf3430886542ca2585363dbe151471b
Mario Perez <mapreal19@gmail.com>
2018-04-18 15:30:50 +0200

    Solution

--- peg_solitaire.rb
+++ peg_solitaire.rb
@@ -40,7 +40,10 @@ class PegSolitaire
   private
 
   def backtracking(matrix, solution, pegs_count, movements)
-    show_solution(solution, movements) if pegs_count == 1
+    if pegs_count == 1
+      show_solution(solution.compact, movements.compact)
+      exit
+    end
 
     (0..6).to_a.each do |i|
       (0..6).to_a.each do |j|
@@ -74,9 +77,120 @@ class PegSolitaire
             solution[pegs_count - 1] = [-1, -1]
             pegs_count += 1
           end
+
+          if can_move_diagonal_left_up?(matrix, i, j)
+            matrix[i][j] = PEG_MISSING
+            matrix[i - 1][j - 1] = PEG_MISSING
+            matrix[i - 2][j - 2] = PEG_PRESENT
+            solution[pegs_count - 1] = [i, j]
+            movements[pegs_count - 1] = '↖'
+            pegs_count -= 1
+            backtracking(matrix, solution, pegs_count, movements)
+            matrix[i][j] = PEG_PRESENT
+            matrix[i - 1][j - 1] = PEG_PRESENT
+            matrix[i - 2][j - 2] = PEG_MISSING
+            solution[pegs_count - 1] = [-1, -1]
+            pegs_count += 1
+          end
+
+          if can_move_up?(matrix, i, j)
+            matrix[i][j] = PEG_MISSING
+            matrix[i - 1][j] = PEG_MISSING
+            matrix[i - 2][j] = PEG_PRESENT
+            solution[pegs_count - 1] = [i, j]
+            movements[pegs_count - 1] = '↑'
+            pegs_count -= 1
+            backtracking(matrix, solution, pegs_count, movements)
+            matrix[i][j] = PEG_PRESENT
+            matrix[i - 1][j] = PEG_PRESENT
+            matrix[i - 2][j] = PEG_MISSING
+            solution[pegs_count - 1] = [-1, -1]
+            pegs_count += 1
+          end
+
+          if can_move_diagonal_right_up?(matrix, i, j)
+            matrix[i][j] = PEG_MISSING
+            matrix[i - 1][j + 1] = PEG_MISSING
+            matrix[i - 2][j + 2] = PEG_PRESENT
+            solution[pegs_count - 1] = [i, j]
+            movements[pegs_count - 1] = '↗'
+            pegs_count -= 1
+            backtracking(matrix, solution, pegs_count, movements)
+            matrix[i][j] = PEG_PRESENT
+            matrix[i - 1][j] = PEG_PRESENT
+            matrix[i - 2][j] = PEG_MISSING
+            solution[pegs_count - 1] = [-1, -1]
+            pegs_count += 1
+          end
+
+          if can_move_down?(matrix, i, j)
+            matrix[i][j] = PEG_MISSING
+            matrix[i + 1][j] = PEG_MISSING
+            matrix[i + 2][j] = PEG_PRESENT
+            solution[pegs_count - 1] = [i, j]
+            movements[pegs_count - 1] = '↓'
+            pegs_count -= 1
+            backtracking(matrix, solution, pegs_count, movements)
+            matrix[i][j] = PEG_PRESENT
+            matrix[i + 1][j] = PEG_PRESENT
+            matrix[i + 2][j] = PEG_MISSING
+            solution[pegs_count - 1] = [-1, -1]
+            pegs_count += 1
+          end
+
+          if can_move_left?(matrix, i, j)
+            matrix[i][j] = PEG_MISSING
+            matrix[i][j - 1] = PEG_MISSING
+            matrix[i][j - 2] = PEG_PRESENT
+            solution[pegs_count - 1] = [i, j]
+            movements[pegs_count - 1] = '←'
+            pegs_count -= 1
+            backtracking(matrix, solution, pegs_count, movements)
+            matrix[i][j] = PEG_PRESENT
+            matrix[i][j - 1] = PEG_PRESENT
+            matrix[i][j - 2] = PEG_MISSING
+            solution[pegs_count - 1] = [-1, -1]
+            pegs_count += 1
+          end
+
+          if can_move_diagonal_left_down?(matrix, i, j)
+            matrix[i][j] = PEG_MISSING
+            matrix[i + 1][j - 1] = PEG_MISSING
+            matrix[i + 2][j - 2] = PEG_PRESENT
+            solution[pegs_count - 1] = [i, j]
+            movements[pegs_count - 1] = '↙'
+            pegs_count -= 1
+            backtracking(matrix, solution, pegs_count, movements)
+            matrix[i][j] = PEG_PRESENT
+            matrix[i + 1][j - 1] = PEG_PRESENT
+            matrix[i + 2][j - 2] = PEG_MISSING
+            solution[pegs_count - 1] = [-1, -1]
+            pegs_count += 1
+          end
         end
       end
     end
+  end
+
+  def can_move_diagonal_right_up?(matrix, i, j)
+    i - 2 > 0 &&
+      j + 2 < 7 &&
+      matrix[i - 1][j + 1] == PEG_PRESENT &&
+      matrix[i - 2][j + 2] == PEG_MISSING
+  end
+
+  def can_move_diagonal_left_up?(matrix, i, j)
+    i - 2 > 0 &&
+      j - 2 > 0 &&
+      matrix[i - 1][j - 1] == PEG_PRESENT &&
+      matrix[i - 2][j - 2] == PEG_MISSING
+  end
+
+  def can_move_diagonal_left_down?(matrix, i, j)
+    i + 2 < 7 &&
+      j - 2 > 0 &&
+      matrix[i + 1][j - 1] == PEG_PRESENT &&
+      matrix[i + 2][j - 2] == PEG_MISSING
   end
 
   def can_move_diagonal_right_down?(matrix, i, j)
@@ -86,20 +200,47 @@ class PegSolitaire
       matrix[i + 2][j + 2] == PEG_MISSING
   end
 
+  def can_move_up?(matrix, i, j)
+    i - 2 > 0 &&
+      matrix[i - 1][j] == PEG_PRESENT &&
+      matrix[i - 2][j] == PEG_MISSING
+  end
+
   def can_move_right?(matrix, i, j)
     j + 2 < 7 &&
       matrix[i][j + 1] == PEG_PRESENT &&
       matrix[i][j + 2] == PEG_MISSING
   end
 
+  def can_move_down?(matrix, i, j)
+    i + 2 < 7 &&
+      matrix[i + 1][j] == PEG_PRESENT &&
+      matrix[i + 2][j] == PEG_MISSING
+  end
+
+  def can_move_left?(matrix, i, j)
+    j - 2 > 0 &&
+      matrix[i][j - 1] == PEG_PRESENT &&
+      matrix[i][j - 2] == PEG_MISSING
+  end
+
   def show_solution(solution, movements)
-    puts solution
-    puts movements
-    puts "solution found!"
+    puts 'Solution found! -->'
+    solution.reverse.each_with_index do |position, index|
+      puts "Position: [#{position[0]}, #{position[1]}], Movement: #{movements.reverse[index]}"
+    end
   end
 
   def build_pegs_matrix
-    pegs_matrix = Hash.new(build_row)
+    pegs_matrix = [
+      build_row,
+      build_row,
+      build_row,
+      build_row,
+      build_row,
+      build_row,
+      build_row
+    ]
 
     OUT_OF_BOARD_POSITIONS.each do |position|
       pegs_matrix[position[0]][position[1]] = PEG_OUTSIDE_BOARD
